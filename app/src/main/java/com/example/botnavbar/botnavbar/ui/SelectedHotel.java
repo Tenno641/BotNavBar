13c1b891dfd685c15d1f6603caa3e9a541ae4b94
Tenno641 <ahmedfotoh2003@gmail.com>
2023-11-07 22:54:49 +0200

    Selected_Hotel_Page

---
+++ app/src/main/java/com/example/botnavbar/botnavbar/ui/SelectedHotel.java
@@ -0,0 +1,16 @@
+package com.example.botnavbar.botnavbar.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.botnavbar.R;
+
+public class SelectedHotel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_hotel_page);
+    }
+}
