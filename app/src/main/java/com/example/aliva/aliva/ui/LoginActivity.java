198cf1ae696778fd4b536265572ca7f1ff2bbc0a
Tenno641 <ahmedfotoh2003@gmail.com>
2023-12-01 16:01:01 +0200

    [ADD] Sign up text

--- app/src/main/java/com/example/aliva/aliva/ui/LoginActivity.java
+++ app/src/main/java/com/example/aliva/aliva/ui/LoginActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.aliva.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button loginButton;
+    TextView signUpText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +20,16 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_activity);
 
         loginButton = findViewById(R.id.loginPageButton);
+        signUpText = findViewById(R.id.signUp_text);
 
         loginButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        signUpText.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
             startActivity(intent);
             finish();
         });
