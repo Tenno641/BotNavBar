fd7934050647334f742ec2dc9acc38934314963c
Tenno641 <ahmedfotoh2003@gmail.com>
2023-12-05 07:05:40 +0200

    [ADD] Admin can add Hotels

--- app/src/main/java/com/example/aliva/aliva/ui/AdminFragment.java
+++ app/src/main/java/com/example/aliva/aliva/ui/AdminFragment.java
@@ -8,15 +8,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.example.aliva.R;
+import com.example.aliva.aliva.adapters.DataBaseAdapter;
+import com.example.aliva.aliva.pojos.HotelModel;
 
 public class AdminFragment extends Fragment {
 
-    Button addButton, updateButton, deleteButton;
+    Button addMainButton, updateMainButton, deleteMainButton, addButton, updateButton, deleteButton;
     LinearLayout addPage, updatePage, deletePage;
+    EditText hotelName, hotelLocation, hotelDescription, hotelPrice, updateHotel, deleteHotel;
 
+    DataBaseAdapter dataBaseAdapter;
     boolean addVisible = false;
     boolean updateVisible = false;
     boolean deleteVisible = false;
@@ -35,33 +40,57 @@ public class AdminFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_admin, container, false);
 
-        addButton = view.findViewById(R.id.addHotel_button);
+        dataBaseAdapter = new DataBaseAdapter(getContext());
 
-        
+        addMainButton = view.findViewById(R.id.addHotel_button);
+        // add page views
+        hotelName = view.findViewById(R.id.nameEditText);
+        hotelLocation = view.findViewById(R.id.locationEditText);
+        hotelDescription = view.findViewById(R.id.DescriptionEditText);
+        hotelPrice = view.findViewById(R.id.priceEditText);
+        addButton = view.findViewById(R.id.submitAddingHotel_button);
 
-        updateButton = view.findViewById(R.id.updateHotel_button);
-        deleteButton = view.findViewById(R.id.deleteHotel_button);
+        updateMainButton = view.findViewById(R.id.updateHotel_button);
+        // update page views
+
+
+        deleteMainButton = view.findViewById(R.id.deleteHotel_button);
+        // delete page views
 
         addPage = view.findViewById(R.id.addHotelPage_layout);
         updatePage = view.findViewById(R.id.updateHotelPage_layout);
         deletePage = view.findViewById(R.id.deleteHotelPage_layout);
 
-        addButton.setOnClickListener(v -> {
+        addMainButton.setOnClickListener(v -> {
             addPage.setVisibility(addVisible ? View.GONE : View.VISIBLE);
             addVisible = !addVisible;
-            addButton.setCompoundDrawablesWithIntrinsicBounds(0, 0, addVisible ? R.drawable.baseline_keyboard_arrow_up_24 : R.drawable.baseline_keyboard_arrow_down_24, 0);
+            addMainButton.setCompoundDrawablesWithIntrinsicBounds(0, 0, addVisible ? R.drawable.baseline_keyboard_arrow_up_24 : R.drawable.baseline_keyboard_arrow_down_24, 0);
         });
 
-        updateButton.setOnClickListener(v -> {
+        updateMainButton.setOnClickListener(v -> {
             updatePage.setVisibility(updateVisible ? View.GONE : View.VISIBLE);
             updateVisible = !updateVisible;
-            updateButton.setCompoundDrawablesWithIntrinsicBounds(0, 0, updateVisible ? R.drawable.baseline_keyboard_arrow_up_24 : R.drawable.baseline_keyboard_arrow_down_24, 0);
+            updateMainButton.setCompoundDrawablesWithIntrinsicBounds(0, 0, updateVisible ? R.drawable.baseline_keyboard_arrow_up_24 : R.drawable.baseline_keyboard_arrow_down_24, 0);
         });
 
-        deleteButton.setOnClickListener(v -> {
+        deleteMainButton.setOnClickListener(v -> {
             deletePage.setVisibility(deleteVisible ? View.GONE : View.VISIBLE);
             deleteVisible = !deleteVisible;
-            deleteButton.setCompoundDrawablesWithIntrinsicBounds(0, 0, deleteVisible ? R.drawable.baseline_keyboard_arrow_up_24 : R.drawable.baseline_keyboard_arrow_down_24, 0);
+            deleteMainButton.setCompoundDrawablesWithIntrinsicBounds(0, 0, deleteVisible ? R.drawable.baseline_keyboard_arrow_up_24 : R.drawable.baseline_keyboard_arrow_down_24, 0);
+        });
+
+        addButton.setOnClickListener(v -> {
+            String name = hotelName.getText().toString();
+            String location = hotelLocation.getText().toString();
+            String description = hotelDescription.getText().toString();
+            String price = hotelPrice.getText().toString();
+            dataBaseAdapter.insertRow(new HotelModel(R.drawable.test, name, location, description, "0.0", price));
+
+            hotelName.setText("");
+            hotelLocation.setText("");
+            hotelDescription.setText("");
+            hotelPrice.setText("");
+
         });
 
         return view;
