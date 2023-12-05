14cc5664caf75fce474f1e9582c4984edaa55e87
Tenno641 <ahmedfotoh2003@gmail.com>
2023-12-05 07:17:44 +0200

    [FIX] Creating new account causing problem

--- app/src/main/java/com/example/aliva/aliva/ui/RegisterActivity.java
+++ app/src/main/java/com/example/aliva/aliva/ui/RegisterActivity.java
@@ -10,20 +10,10 @@ import com.example.aliva.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    Button registerButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
-
-        registerButton = findViewById(R.id.registerPageButton);
-
-        registerButton.setOnClickListener(v -> {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        });
 
     }
 }
